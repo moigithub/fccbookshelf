85924a07f910bf3acb30458f7f322b1af2709bb5
moigithub <mcarlosman@gmail.com>
2015-08-16 12:08:51 -0500

    fix client with new api books

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,26 +2,27 @@
 
 angular.module('basej4booksApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+    $scope.books = [];
+    $scope.newBook = '';
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
+    $http.get('/api/books').success(function(books) {
+      $scope.books = books;
+      socket.syncUpdates('book', $scope.books);
     });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
+    $scope.addBook = function() {
+      if($scope.newBook === '') {
         return;
       }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
+      $http.post('/api/books', { name: $scope.newBook });
+      $scope.newBook = '';
     };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
+    $scope.deleteBook = function(book) {
+      $http.delete('/api/books/' + book._id);
     };
 
     $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
+      socket.unsyncUpdates('book');
     });
   });
