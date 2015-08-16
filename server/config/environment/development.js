876c4eaf13206d0bac557ba91d47a2f965400d04
moigithub <mcarlosman@gmail.com>
2015-08-16 10:08:47 -0500

    first commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/basej4books-dev'
+  },
+
+  seedDB: true
+};
